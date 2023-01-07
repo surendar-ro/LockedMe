ae856d5856d11b2567e9de4a76dca145697712b2
Surendar Vasudevan <surendarvasan1999@gmail.com>
2023-01-07 22:42:20 +0530

    initial commit

---
+++ src/com/lockedme/LockedMeMain.java
@@ -0,0 +1,14 @@
+package com.lockedme;
+
+public class LockedMeMain {
+
+	public static void main(String[] args) {
+
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+
+		MenuOptions.printWelcomeScreen("LockedMe", "Surendar Vasudevan");
+
+		HandleOptions.handleWelcomeScreenInput();
+	}
+}
